74347ffb3390cb3e1e28c839aa87c0c638081e58
Khairunnisa Ramadani <khairunicha123@gmail.com>
2026-08-28 12:11:37 +0700

    Add files via upload

---
+++ Project/Latihan17.java
@@ -0,0 +1,9 @@
+public class Latihan17 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Khai");
+
+        System.out.print("\nBelajar Java itu mudah");
+        //System.out.print("\nSemakin Sering Mencoba, Semakin Jago.");
+    }
+}
