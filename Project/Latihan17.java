b7a403fb5a7c4b97ae2626f03f65dde37f3a081f
Khairunnisa Ramadani <khairunicha123@gmail.com>
2026-09-02 17:49:35 +0700

    Delete Project/Latihan17.java

--- Project/Latihan17.java
+++
@@ -1,9 +0,0 @@
-public class Latihan17 {
-
-    public static void main(String[] args) {
-        System.out.println("Nama Saya Khai");
-
-        System.out.print("\nBelajar Java itu mudah");
-        //System.out.print("\nSemakin Sering Mencoba, Semakin Jago.");
-    }
-}
